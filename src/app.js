75773a190b8d2c05072dbb32ecc29ecd884a9bfe
Munna <mehedihasanmunna516@gmail.com>
2025-12-20 20:20:50 +0600

    update

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(cors(
     {
-        origin: ["http://localhost:3000", "http://localhost:5173", "http://localhost:5174"],
+        origin: "*",
         credentials: true,
     }
 ));
@@ -15,8 +15,8 @@ app.use(cors(
 app.use(
     "/api/v1/stripe",
     express.raw({ type: "application/json" })
-    ,routes
-  );
+    , routes
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
