c12e09e3e3ced9923c2921f1024d880abe1b7288
Munna <mehedihasanmunna516@gmail.com>
2025-12-29 21:17:55 +0600

    updated cors

--- src/app.js
+++ src/app.js
@@ -12,6 +12,8 @@ app.use(
 
 const allowedOrigins = [
   "http://localhost:5173",
+  "http://localhost:5174",
+  "http://localhost:3000",
   "https://soundtrackmynight.com",   // React (CRA)
   "https://m-music-lory.vercel.app" // Production frontend
 ];
